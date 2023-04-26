e222139e31f9ccf31396430f82bbd98052d835d5
YK <1627936869@qq.com>
2023-04-26 21:41:58 +0800

    feat: structure and env preparation

---
+++ gulimall-member/src/test/java/com/york/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.york.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
